53c4bc014cc11c785c39bf61bcc302e49265aead
Obbie254 <obbienjehia@gmail.com>
2024-02-18 12:58:00 +0300

    debug - fix error of last_number when number is negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+"""
+def last_digit(num):
+    last_digit_unsigned = abs(num) % 10
+    return -last_digit_unsigned if (num < 0) else last_digit_unsigned
+"""
+if (number < 0):
+    last_digit = abs(number) % 10
+else:
+    last_digit = number % 10
 if (last_digit > 5):
-    print("Last digit of", number, "is", last_digit, "and is greater than 5")
+    print("Last digit of {} is {} and is greater\
+    than 5".format(number, last_digit))
 elif (last_digit == 0):
-    print("Last digit of", number, "is", last_digit, "and is zero")
+    print("Last digit of {} is {} and is zero".format(number, last_digit))
 elif (last_digit < 6):
-    print("Last digit of", number, "is", last_digit)
-    print("and is less than 6 and not zero")
+    print("Last digit of {} is {} and is less\
+    than 6 and not zero".format(number, last_digit))
 else:
     print("Error")
