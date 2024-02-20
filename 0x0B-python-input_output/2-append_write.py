ea89f0d0e27554ec9b62cdb31c6f864d083a0d70
Obbie254 <obbienjehia@gmail.com>
2024-02-21 02:32:22 +0300

    created file 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Appends a string of a text file"""
+
+
+def append_write(filename="", text=""):
+    """
+    fxn appends a string at the end of a text file (UTF8) and
+    returns the number of characters added
+    """
+    with open(filename, 'a') as f:
+        return f.write(text)
