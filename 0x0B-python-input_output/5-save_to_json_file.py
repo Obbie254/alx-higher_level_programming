42aab34655323c6cc7b13789cd3720e1aafea854
Obbie254 <obbienjehia@gmail.com>
2024-02-21 02:36:09 +0300

    debug file 5 trailing whitespace

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """
-    fxn that writes an Object to a text file, 
+    fxn that writes an Object to a text file,
     using a JSON representation
     """
     with open(filename, 'w') as f:
