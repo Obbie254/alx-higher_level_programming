903fdbb7f25f280f41fa7b620703c7b213d6054d
Obbie254 <obbienjehia@gmail.com>
2024-02-21 03:09:47 +0300

    debug file 11 done

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -12,7 +12,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """ retrieves a dictionary representation """
-        if (type(attrs)) == list and all(type(i) == str for i in attrs):
+        if (type(attrs)) is list and all(type(i) is str for i in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         else:
             return self.__dict__
