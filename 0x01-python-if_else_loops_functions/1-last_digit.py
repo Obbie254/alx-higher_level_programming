c7bddccd29ea9b314bd1c8c4bbdda5e73c7728b1
Obbie254 <obbienjehia@gmail.com>
2024-02-27 19:30:26 +0300

    debug

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,18 +6,20 @@ def last_digit(num):
     last_digit_unsigned = abs(num) % 10
     return -last_digit_unsigned if (num < 0) else last_digit_unsigned
 """
+myStr = "Last digit of {} is {} and is {}"
+str1 = "greater than 5"
+str2 = "0"
+str3 = "less than 6 and not 0"
 if (number < 0):
     last_digit = abs(number) % 10
 else:
     last_digit = number % 10
 
 if (last_digit > 5):
-    print("Last digit of {} is {} and is greater than 5\
-    ".format(number, last_digit))
+    print(myStr.format(number, last_digit, str1))
 elif (last_digit == 0):
-    print("Last digit of {} is {} and is zero".format(number, last_digit))
+    print(myStr.format(number, last_digit, str2))
 elif (last_digit < 6):
-    print("Last digit of {} is {} and is less than 6 and not zero\
-    ".format(number, last_digit))
+    print(myStr.format(number, last_digit, str3))
 else:
     print("Error")
