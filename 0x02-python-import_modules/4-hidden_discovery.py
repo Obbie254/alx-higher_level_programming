db2f7741916392d17efb50a4e47171b018a6c3c3
Obbie254 <obbienjehia@gmail.com>
2024-04-21 19:22:42 +0300

    debugging done on file 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
-#!bin/usr/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    nam = dir(hidden_4)
-    for i in range(len(nam)):
-        if (nam[i][:2] != "__"):
-            print(nam[i])
+    arr = dir(hidden_4)
+    for i in range(len(arr)):
+        if (arr[i][:2] != "__"):
+            print(arr[i])
