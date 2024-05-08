625e5188028861802efaa804391e0d21d6d5d633
Obbie254 <obbienjehia@gmail.com>
2024-05-08 18:54:36 +0300

    created file 1 and it's test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class with a private attribute and instantation"""
+
+
+class Square:
+    """class square"""
+
+    def __init__(self, size):
+        self.__size = size
