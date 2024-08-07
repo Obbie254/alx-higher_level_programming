9b3cad26bf44d40b8c50830d3cc2ce2c2da14279
Obbie254 <obbienjehia@gamil.com>
2024-08-07 07:47:09 +0300

    debug on file 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,21 @@
-#!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if (a < b):
-        return (c)
-    elif (c > b):
-        return (a + b)
-    return ((a * b) - c)
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: linked list
+ * Return: a integer.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *lts = list;
+	listint_t *aux = lts;
+
+	while (lts && aux && lts->next)
+	{
+		aux = aux->next;
+		lts = lts->next->next;
+		if (aux == lts)
+			return (1);
+	}
+	return (0);
+}
