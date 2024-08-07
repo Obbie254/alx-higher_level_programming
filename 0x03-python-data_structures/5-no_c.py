ad244e341c709b54e71e7725b583e392d3194753
Obbie254 <obbienjehia@gamil.com>
2024-08-07 08:03:55 +0300

    created file 8

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+Method 1
+By creating a dict
+"""
+
+
 def no_c(my_string):
     my_string = my_string.translate({ord(i): None for i in 'Cc'})
     return (my_string)
+
+"""
+Method 2
+By creating a table
+
+def no_c(my_string):
+	my_string = my_string.translate(str.maketrans("", "", "cC"))
+	print(my_string)
+"""
